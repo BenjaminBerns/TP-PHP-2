09065882710f01e5640603046e9814829f1a3133
Benji <benjixard@gmail.Com>
2024-11-18 16:03:03 +0100

    Design

--- utils/index.php
+++ utils/index.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Election délégué</title>
+    <link rel="stylesheet" href="../public/styles/app.css" />
+</head>
+<body>
+    <h1 class="Titre">Application éléction des délégués</h1>
+    <h2 class="Titre2">Candidats</h2>
+    <div class="box">
+        <div class="container1">
+            <h3 class="candidat1">Candidat 1</h3>
+            <h4 class="counter1">0</h4>
+            <button class="addbtn1" id="btn">+</button>
+        </div>
+        <div class="container2">
+            <h3 class="candidat2">Candidat 2</h3>
+            <h4 class="counter2">0</h4>
+            <button class="addbtn2" id="btn">+</button>
+        </div>
+    </div>
+</body>
+</html>
